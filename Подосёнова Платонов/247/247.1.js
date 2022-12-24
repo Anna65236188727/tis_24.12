9b2faa071573a1c4171c566fccddac404419b4d0
Anna65236188727 <94472429+Anna65236188727@users.noreply.github.com>
2022-12-24 15:31:41 +0300

    Add files via upload

---
+++ Подосёнова Платонов/247/247.1.js
@@ -0,0 +1,6 @@
+
+let button1 = document.querySelector('#button1');
+let text = document.querySelector('#text')
+button1.addEventListener('click', function () {
+    console.log(text.textContent);
+});
